7bbc5a1d061d1f76934e26f3c9887af4896ef228
Nikolai <nvanbaak@gmail.com>
2020-11-22 00:22:50 -0800

    navbar styling dynamic based on current page

---
+++ public/portfolio-entries.js
@@ -0,0 +1,32 @@
+module.exports = [
+    {
+        title: "CityScoop",
+        imgURL: "./Assets/logo-black-accent.png",
+        github: "CityScoop",
+        desc: "A one-stop shop for all the information you need when moving to a new city."
+    },
+    {
+        title: "Legends of Javascript",
+        imgURL: "./Assets/legends-of-javascript-thumbnail.png",
+        github: "legends-of-javascript",
+        desc: "Want to test your javascript skills against the inevitable robot takeover?  Try this quiz game!"
+    },
+    {
+        title: "Cryptoforge",
+        imgURL: "./Assets/cryptoforge-thumbnail.png",
+        github: "cryptoforge",
+        desc: "It's a really metal-looking password generator, not a bitcoin miner.  Sorry to get your hopes up."
+    },
+    {
+        title: "Command Center",
+        imgURL: "./Assets/command-center-thumbnail.png",
+        github: "command-center",
+        desc: "A customizable homepage replacement site to give easy access to your favorite sites."
+    },
+    {
+        title: "Pocket Butler",
+        imgURL: "./Assets/pocket-butler-thumbnail.png",
+        github: "pocket-butler",
+        desc: "The classiest organizational app you'll ever use.  Pocket Butler was optimized for people with executive dysfunction (e.g. ADHD) to succeed where other apps fail."
+    }
+];
--- src/App.js
+++ src/App.js
@@ -1,8 +1,30 @@
 import React from "react";
-import Header from "./components/header";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Header from "./components/Header";
 
-function App() {
-  return <Header />;
+class App extends React.Component{
+  
+    state = {
+        currentHome: true,
+        // currentPortfolio: false
+    }
+
+    goToHome = () => {
+        this.setState( {currentHome:true} )
+    }
+
+    goToPortfolio = () => {
+        this.setState( {currentHome:false} )
+    }
+
+    render() {
+        return (
+            <Header 
+                currentHome={this.state.currentHome}
+                goToPortfolio={this.goToPortfolio}
+                goToHome={this.goToHome}/>
+            );
+    }  
 }
 
 export default App;

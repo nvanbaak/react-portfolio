d78a8afed2e41d5fe44d57240d0e173f09913f6a
Nikolai <nvanbaak@gmail.com>
2020-11-22 00:49:44 -0800

    navbar works now

--- src/App.js
+++ src/App.js
@@ -6,21 +6,33 @@ class App extends React.Component{
   
     state = {
         currentHome: true,
+        homeClass:"nav-item active",
+        portfolioClass:"nav-item"
         // currentPortfolio: false
     }
 
     goToHome = () => {
-        this.setState( {currentHome:true} )
+        this.setState( {
+            currentHome:true,
+            homeClass:"nav-item active",
+            portfolioClass:"nav-item"
+        } );
     }
 
     goToPortfolio = () => {
-        this.setState( {currentHome:false} )
+        this.setState( {
+            currentHome:false,
+            homeClass:"nav-item",
+            portfolioClass:"nav-item active"
+        } );
     }
 
     render() {
         return (
             <Header 
                 currentHome={this.state.currentHome}
+                homeClass={this.state.homeClass}
+                portfolioClass={this.state.portfolioClass}
                 goToPortfolio={this.goToPortfolio}
                 goToHome={this.goToHome}/>
             );
--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,35 +1,22 @@
 import React from 'react'
-import { Navbar } from "react-bootstrap";
 import './style.css'
 
 export default function header(props) {
 
-
-    let homeIsCurrent = (pageBool) => {
-        return pageBool ? <span className="sr-only">(current)</span> : "";
-    }
-
-    let portfolioIsCurrent = (pageBool) => {  
-        return pageBool ? "" : <span className="sr-only">(current)</span>;
-    }
-
     return (
-
-        <Navbar className="navbar fixed-top navbar-expand-sm navbar-light">
-
+        <nav className="navbar fixed-top navbar-expand-sm navbar-light">
             <h1>Nik Van Baak</h1>
-
             <div className=" navbar-collapse justify-content-end" id="navbarToggler">
             <ul className="navbar-nav mt-2 mt-lg-0">
-                <li className="nav-item active">
-                    <a className="nav-link" href="#">Home {homeIsCurrent(props.homeIsCurrent)}</a>
+                <li className={props.homeClass}>
+                    <a className="nav-link" onClick={props.goToHome} href="#">Home</a>
                 </li>
-                <li className="nav-item">
-                    <a className="nav-link" href="./portfolio.html">Portfolio {portfolioIsCurrent(props.homeIsCurrent)}</a>
+                <li className={props.portfolioClass}>
+                    <a className="nav-link" onClick={props.goToPortfolio} href="#">Portfolio </a>
                 </li>
             </ul>
             </div>
-        </Navbar>
+        </nav>
 
     )
 }
